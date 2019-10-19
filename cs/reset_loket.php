bcf13b033de49f6eb06cee69e7d361b7ee229433
rto <riotriyanto18@gmail.com>
2019-10-19 10:44:01 +0700

    fi

--- cs/reset_loket.php
+++ cs/reset_loket.php
@@ -3,7 +3,7 @@
 	date_default_timezone_set("Asia/Bangkok");
 	$jam = date("H");
 	$jam = (int)$jam;
-	if ($jam > 9 || $jam < 7) {
+	if ($jam > 15 || $jam < 7) {
 		// echo "reset";
 		$query = mysqli_query($koneksi, "UPDATE loket SET status = 'tidak aktif'");     
 		echo json_encode("Reset ok");
